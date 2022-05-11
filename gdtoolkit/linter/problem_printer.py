07100edc162ea6cf5c1fc9b8a81d3f301eca0817
Nathan-Rozendaal <55398618+Nathan-Rozendaal@users.noreply.github.com>
2022-05-11 12:20:26 +0200

    Update problem_printer.py
    Made printer compatible with github actions

--- gdtoolkit/linter/problem_printer.py
+++ gdtoolkit/linter/problem_printer.py
@@ -4,11 +4,11 @@ from .problem import Problem
 
 def print_problem(problem: Problem, file_path: str) -> None:  # TODO: colors
     print(
-        "{}:{}: Error: {} ({})".format(
+        "::error file={},line={},col={}::{}".format(
             file_path,
             problem.line,
-            problem.description,
-            problem.name,
+            problem.column,
+            problem.description
         ),
         file=sys.stderr,
     )
